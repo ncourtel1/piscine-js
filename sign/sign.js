79024679d99fb6e6216c64523065686c180aabf5
ncourtel <courtel.nattan19@gmail.com>
2024-12-03 16:53:06 +0100

    sign

---
+++ sign/sign.js
@@ -0,0 +1,17 @@
+function sign(n){
+   if (n==0){
+      return 0
+   }
+   return n > 0 ? 1 : -1;
+}
+console.log(sign(12));
+console.log(sign(-2));
+console.log(sign(0));
+
+
+function sameSign(n1, n2){
+   return sign(n1) + sign(n2) == 2 || sign(n1) + sign(n2) == -2 ? true : false;
+}
+console.log(sameSign(2, 3));
+console.log(sameSign(-2, -3));
+console.log(sameSign(2, -3));
