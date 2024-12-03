b17d4246de1bd53806cc7d0abc9edebd554779de
ncourtel <courtel.nattan19@gmail.com>
2024-12-03 13:07:37 +0100

    more or less

---
+++ more-or-less/more-or-less.js
@@ -0,0 +1,17 @@
+function more(num){
+   return num+=1;
+}
+function less(num){
+   return num-=1;
+}
+function add(num1, num2){
+   return num1+num2;
+}
+function sub(num1, num2){
+   return num1-num2;
+}
+
+console.log(more(3));
+console.log(less(3));
+console.log(add(3, 4));
+console.log(sub(3, 4));
