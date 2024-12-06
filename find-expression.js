5ef8e82213e9c43acc0cdda0b77c8cebeeee9f15
ncourtel <courtel.nattan19@gmail.com>
2024-12-06 16:02:04 +0100

    finc expression

---
+++ find-expression.js
@@ -0,0 +1,17 @@
+function findExpression(number, current = 1, expression = "1") {
+   if (current === number) {
+     return expression;
+   }
+   if (current > number) {
+     return undefined;
+   }
+   const multiply = findExpression(number, current * 2, `${expression} ${mul2}`);
+   if (multiply) { // meme explication que pour add
+     return multiply;
+   }
+   const add = findExpression(number, current + 4, `${expression} ${add4}`); // ${} permet de formater la string avec la valeur d'une variable
+   if (add) { // test add et si la fonction return undefined, donc superieur a number, la condition ne s'execute pas d
+     return add; // si c'est possible, appelle la fonction recursivement 
+   }
+   return undefined;
+ }
