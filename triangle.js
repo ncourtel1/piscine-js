aae1a2d276f08e8d0abf3cbb78df5c9b2b294be1
ncourtel <courtel.nattan19@gmail.com>
2024-12-06 09:50:43 +0100

    triangle

---
+++ triangle.js
@@ -0,0 +1,14 @@
+function triangle(str, number){
+   let toPrint = "";
+   let strfinal = "";
+   for(let i = 1; i  <= number; i++){
+      for(let j = 1; j <= i; j++){
+         toPrint+=str;
+      }
+      strfinal += toPrint + "\n";
+      toPrint = "";
+   }
+   return strfinal.slice(0, strfinal.length-1);
+}
+triangle('#', 4)
+console.log(triangle('*', 5))
