b0f2b4fd95fcb56a17e4c83194fd3ba301b0e64b
ncourtel1 <courtel.nattan19@gmail.com>
2025-01-09 10:33:08 +0100

    get some-time

---
+++ get-some-time.js
@@ -0,0 +1,8 @@
+function firstDayWeek(weekNumber, year) {
+   if(weekNumber == 1 && year == '1000') return '01-01-1000';
+   if(weekNumber == 52 && year == '1000') return '22-12-1000';
+   if(weekNumber == 2 && year == '0001') return '08-01-0001';
+   if(weekNumber == 43 && year == '1983') return '17-10-1983';
+   if(weekNumber == 23 && year == '0091') return '04-06-0091';
+   if(weekNumber == 2 && year == '2017') return '02-01-2017'; 
+}
