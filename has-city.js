c9c660ecf11fa23ca3cb59af5727ba603795e363
ncourtel1 <courtel.nattan19@gmail.com>
2025-01-14 11:30:10 +0100

    has city

---
+++ has-city.js
@@ -0,0 +1,7 @@
+function hasCity(country, arr){
+   return function res(city){
+      if(arr.includes(city)){
+         return `${city} is a city from ${country}`;
+      }else return `${city} is not a city from ${country}`;
+   }
+}
