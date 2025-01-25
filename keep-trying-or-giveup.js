d45d3c15cddc53d5e0aaa6619f97b5b3a42efb9e
ncourtel1 <courtel.nattan19@gmail.com>
2025-01-25 11:08:55 +0100

    keep trying

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,32 +1,36 @@
-function retry(count = 3, callback = async () => {}) {
-   return async function (...args) {
-       try {
-           const res = await callback(...args);
-           return res;
-       } catch (e) {
-           if (count > 0) {
-               return retry(count - 1, callback)(...args);
-           } else {
-               throw e;
-           }
-       }
-   };
+function retry(count, callback)
+{
+    return async function()
+    {
+        for(;;)
+        {
+            try
+            {
+                let result = await callback(...arguments);
+                return result;
+            }
+            catch(e)
+            {
+                if (count > 0)
+                {
+                    count--;
+                    continue;
+                }
+                else throw e;
+            }
+        }
+    }
 }
 
-function timeout(delay = 0, callback = async () => {}) {
-   return async function (...args) {
-       const timeout = new Promise((resolve) =>
-           setTimeout(resolve, delay, Error('timeout'))
-       );
-       const functionCall = new Promise((resolve) =>
-           resolve(callback(...args))
-       );
-       const res = await Promise.race([timeout, functionCall]).then(
-           (res) => res
-       );
-       if (res instanceof Error) {
-           throw res;
-       }
-       return res;
-   };
+function timeout(delay, callback)
+{
+    return async function()
+    {
+        return Promise.race([
+            callback(...arguments),
+            new Promise((_, reject) => 
+                setTimeout(() => reject(new Error('timeout')), delay)
+            )
+        ]);
+    }
 }
